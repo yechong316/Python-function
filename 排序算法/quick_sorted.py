45d1454c154ab4561921818d87cd5a5f32cf9b2d
Jamie Lannister <18311448483@163.com>
2019-05-27 19:59:39 +0800

    完成快速排序

--- 排序算法/quick_sorted.py
+++ 排序算法/quick_sorted.py
@@ -1,21 +1,41 @@
 import numpy as np
-def qucik_sorted(alist):
 
-    n = len(alist)
-    low = 0
-    high = n -1
-    mid_value = alist[0]
+
+def quick_sorted(list, first, last):
+
+    coutn = 1
+    if first >= last:
+        coutn += 1
+        print('仅有一个元素，无需排序！',coutn)
+        return
+
+    mid_value = list[first]
+
+    low = first
+    high = last
 
     while low < high:
-        while low < high and alist[low] < mid_value:
 
-            alist[]
-    pass
+        while low < high and list[high] >= mid_value:
+            high -= 1
+        list[low] = list[high]
+
+        while low < high and list[low] < mid_value:
+            low += 1
+        list[high] = list[low]
+
+
+    list[low] = mid_value
+#   对左边排序
+    quick_sorted(list, first, low - 1)
+    quick_sorted(list, low + 1, last)
+
+    return list
 
 if __name__ == '__main__':
     n_samples = 9
     alist = np.random.randint(-20, 20, size=n_samples)
     print('排序前：', alist)
-    l = qucik_sorted(alist)
+    l = quick_sorted(alist, 0, len(alist) - 1)
 
-    print('排序后：', alist)
+    print('排序后：', l)
