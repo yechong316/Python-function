34b4c8cc9baae4f2c7f92ef879c01d0eeec04a7c
Jamie Lannister <18311448483@163.com>
2019-05-31 17:25:19 +0800

    新增部分注释

--- 约瑟夫问题.py
+++ 约瑟夫问题.py
@@ -7,50 +7,54 @@
 罗马的一方，而不是杀了自己。
 '''
 
-# 解决思路，将士兵组合为循环链表，循环遍历，计数器等于7就删除当前节点，直至当前链表长度为1
+
 from 线性表.double_recurrent_LL import *
 
 
 def Josef(list, num=7):
+    '''
+    解决思路，将士兵组合为循环链表，循环遍历，计数器等于7就删除当前节点，直至当前链表长度为1
+    :param list: 所有士兵组成的链表
+    :param num:  每隔几个士兵开始自杀
+    :return: 返回幸存的最后一个士兵
+    '''
 
-
+    # 将所有士兵组成的列表转换为双向循环链表
     ll = Double_link_list()
     ll.list2ll(list)
 
-    length = ll.length()
-
+    # 当链表长度为1时，退出循环
     count = 0
     cur = ll._head
-    first = ll._head
     while ll.length() > 1:
 
-        last_node = ll.final_node()
         # 定义首节点
-
+        last_node = ll.final_node()
         # 开始遍历节点
         cur = cur.next
         count += 1
 
+        # 当计数器到7时，进入删除人的截断
         if count == num:
             count = 1
             # 当被删除节点是链表首节点时，需要将末尾节点的指针指向第二个节点，_head指向第二个节点
             if cur == ll._head:
 
-
                 next_node = cur.next
-
                 ll._head = next_node
+
                 next_node.pre = ll._head
                 last_node.next = next_node
                 print('链表长度为{}, 删除节点号码：{}'.format(ll.length(), cur.ele))
 
+            # 当被删除节点是链表尾节点时，需要将末尾前面的节点的指针指向第一个节点，
             elif cur == last_node:
                 
                 pre_node = cur.pre
-
                 pre_node.next = last_node
                 print('链表长度为{}, 删除节点号码：{}'.format(ll.length(), cur.ele))
 
+            # 当被删除节点是链表中间节点时，直接取出中间的前后节点，定义二者之间的关系即可
             else:
 
                 pre_node = cur.pre
@@ -59,9 +63,6 @@ def Josef(list, num=7):
                 pre_node.next = next_node
                 next_node.pre = pre_node
                 print('链表长度为{}, 删除节点号码：{}'.format(ll.length(), cur.ele))
-
-        length = ll.length()
-
 
     return ll._head
 
